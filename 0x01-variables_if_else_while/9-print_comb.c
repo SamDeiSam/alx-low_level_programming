4ed498354cd6dbf4652007d98df95a22bbddd461
SamDeiSam <pdeisam@gmail.com>
2022-03-11 19:00:12 -0800

    Possible combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -17,12 +17,12 @@ int main(void)
 		putchar(d);
 		if (d != '9')
 		{
-			putchar(',')
+			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar('\n')
+	putchar('\n');
 
-		return (0);
+	return (0);
 }
 
