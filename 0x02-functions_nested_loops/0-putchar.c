c514fc3833997f70edaeed9bc59ac844945fb95c
SamDeiSam <pdeisam@gmail.com>
2022-03-15 14:35:10 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - prints the word "Holberton", followed by a new line.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char w[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(w[i]);
+	_putchar('\n');
+
+	return (0);
+}
