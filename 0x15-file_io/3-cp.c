5d5fbb0c32c566903eb79b848edc743e11fdc4a8
SamDeiSam <pdeisam@gmail.com>
2022-04-29 15:14:48 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
 		if (b < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
-		exit(100)
+		exit(100);
 	}
 	return (0);
 }
