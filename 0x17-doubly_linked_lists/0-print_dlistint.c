28762c87d2b8ef2216cd944f8bde3aa7899b821f
SamDeiSam <pdeisam@gmail.com>
2022-06-09 15:23:57 -0700

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,12 +13,12 @@ size_t print_dlistint(const dlistint_t *h)
 	if (!h)
 		return (0);
 
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			nodes++;
-		}
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
 
-		return (nodes);
+	return (nodes);
 }
