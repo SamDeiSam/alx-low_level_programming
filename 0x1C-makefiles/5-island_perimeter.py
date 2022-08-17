b092a34144c347a2abb11cd1d2c15c60626d1628
SamDeiSam <pdeisam@gmail.com>
2022-08-17 20:04:58 +0100

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,83 +1,31 @@
 #!/usr/bin/python3
-"""
-Implementation of function island_perimeter
-"""
+"""Module that calculates the perimeter of an island in a grid."""
+
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
 
 
 def island_perimeter(grid):
-    """
-    returns the perimeter of the island described in grid
-    """
-    s = -1
-    for ls in grid:
-        for i in ls:
-            if i == 1:
-                s = grid.index(ls)
-                break
-        if s >= 0:
-            break
-    end = -1
-    for ls in grid[s:]:
-        for i in ls:
-            if i == 1:
-                end = 0;
-        if end == 0:
-            end = -1
-            continue
-        else:
-            end = grid[s:].index(ls)
-    if end == -1:
-        end = len(grid) - 1
+    """Returns the perimeter of the island in grid."""
 
-    p = 0
-    c = s + 1
-    width = len(grid[0])
-    while c <= end + 1:
-        k = 0
-        for j in grid[c-1]:
-            if j == 0:
-                k += 1
-            elif j == 1:
-                break
-        i = k
-        if c == 1:
-            while i < width:
-                if grid[c-1][i] == 1 and i == k:
-                    p += 1
-                if grid[c-1][i] == 1 and grid[c][i] == 0:
-                    p += 1
-                if grid[c-1][i] == 1 and i+1 == width:
-                    p += 1
-                elif grid[c-1][i] == 1 and grid[c-1][i+1] == 0:
-                    p += 1
-                p += 1
-                i += 1
+    perim = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perim += num_water_neighbors(grid, i, j)
 
-        elif c == len(grid):
-            while i < width:
-                if grid[c-1][i] == 1 and i == k:
-                    p += 1
-                if grid[c-1][i] == 1 and grid[c-2][i] == 0:
-                    p += 1
-                if grid[c-1][i] == 1 and i+1 == width:
-                    p += 1
-                elif grid[c-1][i] == 1 and grid[c-1][i+1] == 0:
-                    p += 1
-                p += 1
-                i += 1
-
-        else:
-            while i < width:
-                if grid[c-1][i] == 1 and i == k:
-                    p += 1
-                if grid[c-1][i] == 1 and grid[c-2][i] == 0:
-                    p += 1
-                if grid[c-1][i] == 1 and grid[c][i] == 0:
-                    p += 1
-                if grid[c-1][i] == 1 and i+1 == width:
-                    p += 1
-                elif grid[c-1][i] == 1 and grid[c-1][i+1] == 0:
-                    p += 1
-                i += 1
-        c += 1
-    return p
+    return perim
